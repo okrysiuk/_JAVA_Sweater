5cee22ab37fa9aebab88ef1c65f660dc67c9ad63
okrysiuk <oleksandr.krysiuk@gmail.com>
2020-07-21 10:09:51 +0300

    Lesson 5 complited Project moved to Freemarker

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -27,9 +27,15 @@ public class MainController {
     private MessageRepo messageRepo;
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -45,17 +51,6 @@ public class MainController {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
 
-        return "main";
-    }
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter,  Map<String, Object> model) {
-        Iterable<Message> messages;
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-        model.put("messages", messages);
         return "main";
     }
 
